caa7b4f851d157fa8774809e74b098fc8050b64e
Jensfrank <everett7623@gmail.com>
2024-07-18 14:37:44 +0800

    Update Nlbench_beta.sh

--- Nlbench_beta.sh
+++ Nlbench_beta.sh
@@ -8,7 +8,8 @@ VERSION_URL="https://raw.githubusercontent.com/everett7623/nodeloc_vps_test/main
 # 定义颜色
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;36m'
 NC='\033[0m' # No Color
 
 # 定义渐变颜色数组
